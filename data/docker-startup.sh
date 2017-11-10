6d0930cddb75cfc52697dac5c594166a3600bd52
Michael <siepieuk@hotmail.co.uk>
2017-11-10 20:56:53 +0000

    Adding env variable for http port

--- data/docker-startup.sh
+++ data/docker-startup.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 chown -R www-data:www-data /owncloud/config && chmod -R 777 /owncloud/config
+
+export HTTP_PORT=80
+if [[ ! -z $HTTP_WEB_PORT ]]; then
+    export HTTP_PORT=$HTTP_WEB_PORT
+fi
+
+sed -i "s/HTTP_PORT/$HTTP_PORT/g" "/etc/apache2/sites-available/default.conf"
 
 service apache2 start
