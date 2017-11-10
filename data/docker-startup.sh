cd94ce2e8e42abe4516adce070ca26c0cea00935
Michael Bramsiepe <siepieuk@hotmail.co.uk>
2017-11-10 23:52:23 +0000

    Adding apache2 alias support to HTTP_ALIAS

--- data/docker-startup.sh
+++ data/docker-startup.sh
@@ -7,6 +7,13 @@ if [[ ! -z $HTTP_WEB_PORT ]]; then
     export HTTP_PORT=$HTTP_WEB_PORT
 fi
 
+export ALIAS_STRING=
+if [[ ! -z $HTTP_ALIAS ]]; then
+    export ALIAS_STRING="Alias $HTTP_ALIAS /owncloud/owncloud"
+fi
+
 sed -i "s/HTTP_PORT/$HTTP_PORT/g" "/etc/apache2/sites-available/default.conf"
+sed -i "s:ALIAS_STRING:$ALIAS_STRING:g" "/etc/apache2/sites-available/default.conf"
 
 service apache2 start
+
